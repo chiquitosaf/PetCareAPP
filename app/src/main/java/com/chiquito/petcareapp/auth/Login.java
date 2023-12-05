bedaa2774ace487abad8e0860591fa3c842e82a8
chiquitosaf <chiquitosaf@gmail.com>
2023-12-05 20:49:09 +0700

    tambah firebase dan implementasi auth dan database ke registrasi dan login

--- app/src/main/java/com/chiquito/petcareapp/auth/Login.java
+++ app/src/main/java/com/chiquito/petcareapp/auth/Login.java
@@ -7,17 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.chiquito.petcareapp.Home;
 import com.chiquito.petcareapp.R;
 import com.chiquito.petcareapp.onboarding.OnBoarding;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    TextInputEditText email, password;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
 
         Button btnLogin =  findViewById(R.id.btn_login);
         ImageButton btnBack = findViewById(R.id.btn_back);
@@ -25,9 +36,7 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go = new Intent(Login.this, Home.class);
-                startActivity(go);
-                finish();
+                loginUser();
             }
         });
 
@@ -40,5 +49,26 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void loginUser() {
+        String emailText = email.getText().toString();
+        String passwordText = password.getText().toString();
+
+        if (!emailText.isEmpty() && !passwordText.isEmpty()) {
+            auth.signInWithEmailAndPassword(emailText, passwordText)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(Login.this, Home.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(Login.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else{
+            email.setError("Email tidak boleh kosong");
+            password.setError("Password tidak boleh kosong");
+        }
     }
 }
--- app/src/main/java/com/chiquito/petcareapp/auth/Register.java
+++ app/src/main/java/com/chiquito/petcareapp/auth/Register.java
@@ -1,35 +1,58 @@
 package com.chiquito.petcareapp.auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.chiquito.petcareapp.Home;
 import com.chiquito.petcareapp.R;
+import com.chiquito.petcareapp.models.UserModel;
 import com.chiquito.petcareapp.onboarding.OnBoarding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
-    Button btnDaftar;
-    ImageButton btnBack;
+    private Button btnDaftar;
+    private ImageButton btnBack;
+    private TextInputEditText nama, email, password, confirmPassword;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // initialize firebase auth
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance("https://petcareapp-85cfe-default-rtdb.asia-southeast1.firebasedatabase.app");
+
         btnDaftar = findViewById(R.id.btn_daftar);
         btnBack = findViewById(R.id.btn_back);
+        nama = findViewById(R.id.nama_register);
+        email = findViewById(R.id.email_register);
+        password = findViewById(R.id.password_register);
 
         // register button action when clicked
         btnDaftar.setOnClickListener(new View.OnClickListener() {
             //open login activity
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Register.this, Login.class));
+                createUser();
             }
         });
 
@@ -43,5 +66,57 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void createUser() {
+        String userNama = nama.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userNama)) {
+            nama.requestFocus();
+            nama.setError("Nama tidak boleh kosong");
+            return;
+
+        }
+
+        if (TextUtils.isEmpty(userEmail)) {
+            email.requestFocus();
+            email.setError("Email tidak boleh kosong");
+            return;
+
+        }
+
+        if (TextUtils.isEmpty(userPassword)){
+            password.requestFocus();
+            password.setError("Password tidak boleh kosong");
+            return;
+        }
+
+        if (userPassword.length() < 6){
+            password.requestFocus();
+            password.setError("Password harus lebih dari 6 karakter");
+            return;
+        }
+
+        //Create User
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //Check if success
+                        if (task.isSuccessful()) {
+                            //Save user to database
+                            UserModel userModel = new UserModel(userNama, userEmail, userPassword);
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Customer").child(id).setValue(userModel);
+                            Toast.makeText(Register.this, "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Register.this, Login.class));
+                            finish();
+                        } else {
+                            Toast.makeText(Register.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
